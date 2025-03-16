4959e92026b135a5559d6afa6fb2000fb7c94900
osamikoyo <v.pylayev@mail.ru>
2025-03-16 10:56:24 +0500

    add sender manager

--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -2,10 +2,13 @@ package sender
 
 import (
 	"context"
+	"sync"
 
+	"github.com/bytedance/sonic"
 	"github.com/koyo-os/notify-system/internal/config"
 	"github.com/koyo-os/notify-system/internal/models"
 	"github.com/koyo-os/notify-system/pkg/logger"
+	"go.uber.org/zap/zapcore"
 )
 
 type Sender interface{
@@ -17,9 +20,11 @@ type SenderManager struct{
 	senders []Sender
 	logger *logger.Logger
 	cfg *config.Config
+	wg *sync.WaitGroup
+	ctx context.Context
 }
 
-func Init(cfg *config.Config, ch chan []byte, logger *logger.Logger) *SenderManager {
+func Init(cfg *config.Config, ch chan []byte, logger *logger.Logger, wg *sync.WaitGroup) *SenderManager {
 	var senders []Sender
 
 	if cfg.TgCfg.Use {
@@ -37,5 +42,39 @@ func Init(cfg *config.Config, ch chan []byte, logger *logger.Logger) *SenderMana
 		cfg: cfg,
 		logger: logger,
 		senders: senders,
+		wg: wg,
+	}
+}
+
+func (s *SenderManager) Listen() {
+	for {
+		msg := <- s.MessageChan
+		
+		var notify models.Notify
+
+		if err := sonic.Unmarshal(msg, &notify);err != nil{
+			s.logger.Error("cant unmarshal notify", zapcore.Field{
+				Key: "err",
+				String: err.Error(),
+			})
+
+			return
+		}
+
+		for _, sender := range s.senders{
+			s.wg.Add(1)
+			go func() {
+				if err := sender.Send(notify, s.ctx);err != nil{
+					s.logger.Error("cant send notify", zapcore.Field{
+						Key: "err",
+						String: err.Error(),
+					})
+				}
+
+				s.wg.Done()
+			}()
+		}
+
+		s.wg.Wait()
 	}
 }
