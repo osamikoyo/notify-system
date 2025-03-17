b219a4cccd2d6ac1c78cad9c34cb4913cb710708
osamikoyo <v.pylayev@mail.ru>
2025-03-17 07:23:15 +0500

    app end

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,16 +2,23 @@ package app
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
+	"github.com/koyo-os/notify-system/internal/config"
+	"github.com/koyo-os/notify-system/internal/consumer"
+	"github.com/koyo-os/notify-system/internal/sender"
 	"github.com/koyo-os/notify-system/pkg/logger"
+	"go.uber.org/zap/zapcore"
 )
 
 func App() error {
 	counter := 0
 
-	var timerChan chan struct{}
-
+	var (
+		messageChan chan []byte
+		wg sync.WaitGroup
+	)
 	go func ()  {
 		counter++
 
@@ -22,7 +29,7 @@ func App() error {
 		time.Sleep(time.Second)
 	}()
 
-	fmt.Println(`
+	fmt.Print(`
  __    _  _______  _______  ___   _______  __   __ 
 |  |  | ||       ||       ||   | |       ||  | |  |
 |   |_| ||   _   ||_     _||   | |    ___||  |_|  |
@@ -37,4 +44,42 @@ func App() error {
 	logger.Info("start notify...")
 
 	logger.Info("getting config.yaml...")
+
+	cfg, err := config.Init()
+	if err != nil{
+		logger.Error("cant get config, stopped...", zapcore.Field{
+			Key: "err",
+			String: err.Error(),
+		})
+		return err
+	}
+
+	logger.Info("get consumer...")
+
+	consumer, err := consumer.Init(cfg, messageChan)
+	if err != nil{
+		logger.Error("error get consumer, stopped", zapcore.Field{
+			Key: "err",
+			String: err.Error(),
+		}, zapcore.Field{
+			Key: "url",
+			String: cfg.KafkaUrl,
+		})
+	}
+
+	logger.Info("get masanger..,")
+
+	manager := sender.Init(
+		cfg,
+		messageChan,
+		logger,
+		&wg,
+	)
+
+	logger.Info("start listeners...")
+
+	manager.Listen()
+	go consumer.Listen()
+
+	return nil
 }
